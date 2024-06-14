15f6b9bcf064acfc39cb0ae31dbbaafc6ecd49ca
Akiko0210 <qea643@gmail.com>
2024-06-14 12:44:13 -0700

    basic layout

--- front/app/ingame/page.tsx
+++ front/app/ingame/page.tsx
@@ -1,5 +1,42 @@
 import Link from "next/link";
 
+const LeaderBoard = ({ classname }: { classname?: string }) => {
+  const users = [
+    {
+      username: "Tom",
+      score: 35,
+    },
+    {
+      username: "Clayton",
+      score: 30,
+    },
+    {
+      username: "Akiko",
+      score: 20,
+    },
+    {
+      username: "Jessy",
+      score: 12,
+    },
+    {
+      username: "Bat",
+      score: 10,
+    },
+  ];
+  return (
+    <div className={classname}>
+      <div>Leaderboard</div>
+      <div>
+        {users.map((user, index) => {
+          const { username, score } = user;
+          const line = "" + index + ". " + username + " - " + score;
+          return <div>{line}</div>;
+        })}
+      </div>
+    </div>
+  );
+};
+
 const Page = () => {
   return (
     <div className="relative flex items-center justify-center h-screen">
@@ -9,7 +46,9 @@ const Page = () => {
       <div className="">
         <div>Find the x if 2x = 10.</div>
         <textarea />
+        <div className="button flex-none"> submit </div>
       </div>
+      <LeaderBoard classname="absolute top-20 right-10" />
     </div>
   );
 };
--- front/app/page.tsx
+++ front/app/page.tsx
@@ -3,9 +3,13 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      Mathforces
-      <Link href="/ingame">Join the battle</Link>
+    <div className="h-screen flex justify-center items-center flex-col">
+      <div className="h-[200px] flex flex-col justify-between items-center border-white border-[1px] rounded-md p-5">
+        Mathforces
+        <Link className="button" href="/ingame">
+          Join the battle
+        </Link>
+      </div>
     </div>
   );
 }
